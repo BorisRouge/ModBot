9e3f9225342896c1d80bce867999b093ce9cee33
BorisRouge <fkolotov@gmail.com>
2023-01-23 06:03:31 +0700

    proper rulecheck

--- handlers/user.py
+++ handlers/user.py
@@ -4,15 +4,22 @@ from bot import bot, dp, rm, log
 
 async def handle(message: Message):
     log.info(message.text)
+    log.info(type(message.text), [message.text])
+
     if rm.is_vacancy(message.text):
         result = rm.check(message.text)
         log.info(result)
-        if result[0]:
+        if result[0]:  # TODO: Tresult.passed()
             pass
         else:
-            await message.reply(
-                f'you have fucked up with the following:\n' # TODO: форматнуть в список
-                f'{result[1]}')
+            body = ['Похоже, что у оформление вашей вакансии'
+                    ' не соответствует правилам.\n'
+                    'Ссылка на правила — https://t.me/python_vacancy/66\n'
+                    'Проверьте по контрольному листу:\n\n']
+            for item in result[1]:
+                body.append(f'{item[0]:<10}{"—":^10}{item[1]} \n')  # change in rules.RuleManager.check() - True = OK, False = X
+            text = ''.join(body)
+            await message.reply(text)
 
 
 def register_user(d: dp):
--- utils/rules.py
+++ utils/rules.py
@@ -1,33 +1,26 @@
 from utils import data
 
-
 class RuleManager:
-    #  def refresh_rules
+    # TODO:  def refresh_rules
     def __init__(self):
         self.rules = data.rules
 
     def check(self, text) -> (bool, list):
-
-        print(data.rules, data.format.markers)
-
         result = True
         checklist = []
         for rule in self.rules:
+            no_markers = True
             for marker in rule.markers:
-                if marker in text:
-                    checklist.append((rule.name, True))
+                if marker in text.lower():
+                    checklist.append((rule.name, 'OK'))
+                    no_markers = False
                     break
-                else:
-                    checklist.append((rule.name, False))
-                    result = False
-                    break
+            if no_markers:
+                checklist.append((rule.name, 'X'))
+                result = False
         return result, checklist
 
     def is_vacancy(self, text):
-        if '#Вакансия' in text:  #TODO: накидать вариаций
-            return True
-
-
-
-
-
+        for word in data.vacancy:
+            if word in text.lower():
+                return True
